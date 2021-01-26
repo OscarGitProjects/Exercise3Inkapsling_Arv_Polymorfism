835eed0b7c7ba380c637383599f92ad11de1a022
Oscar Andersson <oscarandersson2@gmail.com>
2021-01-26 09:11:02 +0100

    Add project files.

---
+++ Exercise3Inkapsling_Arv_Polymorfism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise3Inkapsling_Arv_Polymorfism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
